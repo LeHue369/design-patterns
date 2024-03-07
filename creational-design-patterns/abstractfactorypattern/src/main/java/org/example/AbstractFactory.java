af045a5e102448cb1f9bcb274242903114f14ed1
Lethihue030699 <lethihue030699@gmail.com>
2024-03-07 16:25:58 +0700

    add abstract factory design pattern

---
+++ creational-design-patterns/abstractfactorypattern/src/main/java/org/example/AbstractFactory.java
@@ -0,0 +1,23 @@
+package org.example;
+
+abstract class AbstractFactory {
+	private static final EmberToolkit EMBER_TOOLKIT = new EmberToolkit();
+	private static final EnginolaToolkit ENGINOLA_TOOLKIT = new EnginolaToolkit();
+
+	static AbstractFactory getFactory(Architecture architecture) {
+		AbstractFactory factory = null;
+		switch (architecture) {
+			case ENGINOLA:
+				factory = ENGINOLA_TOOLKIT;
+				break;
+			case EMBER:
+				factory = EMBER_TOOLKIT;
+				break;
+		}
+		return factory;
+	}
+
+	public abstract CPU createCPU();
+
+	public abstract MMU createMMU();
+}
---
+++ creational-design-patterns/abstractfactorypattern/src/main/java/org/example/Architecture.java
@@ -0,0 +1,5 @@
+package org.example;
+
+enum Architecture {
+	ENGINOLA, EMBER
+}
